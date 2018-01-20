ff5a9b4ab93cd5602f5540706884842141efe2b5
Ryan Lee <gxlhybh@gmail.com>
2018-01-20 21:46:25 +0800

    fix some bugs

--- module/judger.js
+++ module/judger.js
@@ -10,156 +10,157 @@ log4js.connectLogger(logger, {level: 'info'});
 let account = require('./include/account');
 
 class Judger {
-    constructor(config, account, proxy, oj_name) {
-        this.proxy = proxy;
-        this.account = account;
-        this.config = config;
-        this.url = config['url'][oj_name];
-        this.oj_name = oj_name;
-        this.cookie = "";
-        this.ojmodule = require("./include/" + this.oj_name + "_module");
-        this.finished = false;
-        logger.info(`constructed Judger`);
-    }
+	constructor(config, account, proxy, oj_name) {
+		this.proxy = proxy;
+		this.account = account;
+		this.config = config;
+		this.url = config['url'][oj_name];
+		this.oj_name = oj_name;
+		this.cookie = "";
+		this.ojmodule = require("./include/" + this.oj_name + "_module");
+		this.finished = false;
+		logger.info(`constructed Judger`);
+	}
 
-    static valid_judge(err, response) {
-        if (err || response.text.match("wwwcache1.hdu.edu.cn") !== -1) {
-            logger.warn(err || "Server cannot work");
-            return false;
-        }
-        else {
-            return true;
-            //this.login();
-        }
-    }
+	static valid_judge(err, response) {
+		if (err || response.text.match("wwwcache1.hdu.edu.cn") !== -1) {
+			logger.warn(err || "Server cannot work");
+			return false;
+		}
+		else {
+			return true;
+			//this.login();
+		}
+	}
 
-    record(rows) {
-        let accpeted = parseInt(rows[0]['accepted']) + 1;
-        query("update vjudge_problem set accepted=? where problem_id=? and source=?", [accpeted, this.pid, this.oj_name.toUpperCase()]);
-    }
+	record(rows) {
+		let accpeted = parseInt(rows[0]['accepted']) + 1;
+		query("update vjudge_problem set accepted=? where problem_id=? and source=?", [accpeted, this.pid, this.oj_name.toUpperCase()]);
+	}
 
-    async connect(err, response) {
-        if (err) {
-            logger.fatal(err);
-        }
-        try {
+	async connect(err, response) {
+		if (err) {
+			logger.fatal(err);
+		}
+		try {
 
-            const sqlArr = this.ojmodule.format(response, this.sid);
-            const status = sqlArr[1];
-            this.result = sqlArr;
-            query("update vjudge_solution set runner_id=?,result=?,time=?,memory=? where solution_id=?", sqlArr);
-            if (status > 3) {
-                this.finished = true;
-                account[this.oj_name].push(this);
-                if (status === 4) {
-                    query("select accepted from vjudge_problem where problem_id=? and source=?", [this.pid, this.oj_name.toUpperCase()])
-                        .then((rows) => {
-                            this.record(rows);
-                        })
-                    ;
-                }
-            }
-            else {
-                await sleep(500);
-                this.updateStatus(this.pid, this.sid, this.cookie);
-            }
-        }
-        catch (e) {
-            logger.fatal(e);
-            account[this.oj_name].push(this);
-        }
-    };
+			const sqlArr = this.ojmodule.format(response, this.sid);
+			const status = sqlArr[1];
+			this.result = sqlArr;
+			query("update vjudge_solution set runner_id=?,result=?,time=?,memory=? where solution_id=?", sqlArr);
+			if (status > 3) {
+				this.finished = true;
+				account[this.oj_name].push(this);
+				if (status === 4) {
+					query("select accepted from vjudge_problem where problem_id=? and source=?", [this.pid, this.oj_name.toUpperCase()])
+						.then((rows) => {
+							this.record(rows);
+						})
+					;
+				}
+			}
+			else {
+				await sleep(500);
+				this.updateStatus(this.pid, this.sid, this.cookie);
+			}
+		}
+		catch (e) {
+			logger.fatal(e);
+			account[this.oj_name].push(this);
+			this.error();
+		}
+	};
 
-    updateStatus(pid, cookie) {
-        if (this.proxy.length > 4)
-            superagent.get(this.ojmodule.updateurl(pid, this.account)).set("Cookie", cookie).proxy(this.proxy).set(this.config['browser']).end((err, response) => {
-                this.connect(err, response)
-            });
-        else
-            superagent.get(this.ojmodule.updateurl(pid, this.account)).set("Cookie", cookie).set(this.config['browser']).end((err, response) => {
-                this.connect(err, response)
-            });
-    };
+	updateStatus(pid, cookie) {
+		if (this.proxy.length > 4)
+			superagent.get(this.ojmodule.updateurl(pid, this.account)).set("Cookie", cookie).proxy(this.proxy).set(this.config['browser']).end((err, response) => {
+				this.connect(err, response)
+			});
+		else
+			superagent.get(this.ojmodule.updateurl(pid, this.account)).set("Cookie", cookie).set(this.config['browser']).end((err, response) => {
+				this.connect(err, response)
+			});
+	};
 
-    async submitAction() {
-        await sleep(500);
-        logger.info("PID:" + this.pid + " come to update");
-        this.updateStatus(this.pid, this.cookie);
-    };
+	async submitAction() {
+		await sleep(500);
+		logger.info("PID:" + this.pid + " come to update");
+		this.updateStatus(this.pid, this.cookie);
+	};
 
-    submitAnswer(pid, lang, code, cookie) {
-        const postmsg = this.ojmodule.post_format(pid, lang, code);
-        if (this.proxy.length > 4)
-            superagent.post(this.url.post_url).set("Cookie", cookie).set(this.config['browser']).proxy(this.proxy).send(postmsg).end((err, response) => {
-                this.submitAction(err, response)
-            });
-        else
-            superagent.post(this.url.post_url).set("Cookie", cookie).set(this.config['browser']).send(postmsg).end((err, response) => {
-                this.submitAction(err, response)
-            });
-    };
+	submitAnswer(pid, lang, code, cookie) {
+		const postmsg = this.ojmodule.post_format(pid, lang, code);
+		if (this.proxy.length > 4)
+			superagent.post(this.url.post_url).set("Cookie", cookie).set(this.config['browser']).proxy(this.proxy).send(postmsg).end((err, response) => {
+				this.submitAction(err, response)
+			});
+		else
+			superagent.post(this.url.post_url).set("Cookie", cookie).set(this.config['browser']).send(postmsg).end((err, response) => {
+				this.submitAction(err, response)
+			});
+	};
 
-    loginAction(err, response, cookie) {
-        if (err) {
-            logger.fatal(err);
-        }
-        try {
-            this.submitAnswer(this.pid, this.language, this.code, cookie);
-        }
-        catch (e) {
-            logger.fatal(e);
-            this.error();
-        }
-    }
+	loginAction(err, response, cookie) {
+		if (err) {
+			logger.fatal(err);
+		}
+		try {
+			this.submitAnswer(this.pid, this.language, this.code, cookie);
+		}
+		catch (e) {
+			logger.fatal(e);
+			this.error();
+		}
+	}
 
-    error() {
-        query("update vjudge_solution set result='0' where solution_id=?", [this.sid]);
-        account[this.oj_name].push(this);
-    }
+	error() {
+		query("update vjudge_solution set result='0' and runner_id='0' where solution_id=?", [this.sid]);
+		account[this.oj_name].push(this);
+	}
 
-    getAccount() {
-        return this.account;
-    }
+	getAccount() {
+		return this.account;
+	}
 
-    login() {
-        setTimeout(() => {
-            if (!this.finished) {
-                this.error();
-            }
-        }, 1000 * 60 * 2);
-        if (this.proxy.length > 4)
-            superagent.post(this.url.login_url).set(this.config['browser']).proxy(this.proxy).send(this.account).end((err, response) => {
-                this.loginAction(err, response, response.headers["set-cookie"]);
-            });
-        else
-            superagent.post(this.url.login_url).set(this.config['browser']).send(this.account).end((err, response) => {
-                this.loginAction(err, response, response.headers["set-cookie"]);
-            });
-    };
+	login() {
+		setTimeout(() => {
+			if (!this.finished) {
+				this.error();
+			}
+		}, 1000 * 60 * 2);
+		if (this.proxy.length > 4)
+			superagent.post(this.url.login_url).set(this.config['browser']).proxy(this.proxy).send(this.account).end((err, response) => {
+				this.loginAction(err, response, response.headers["set-cookie"]);
+			});
+		else
+			superagent.post(this.url.login_url).set(this.config['browser']).send(this.account).end((err, response) => {
+				this.loginAction(err, response, response.headers["set-cookie"]);
+			});
+	};
 
-    async website_valid_check() {
-        let valid = true;
-        if (this.proxy.length > 4)
-            await superagent.get(this.url.url).set(this.config['browser']).proxy(this.proxy).end((err, response) => {
-                valid = Judger.valid_judge(err, response);
-            });
-        else
-            await superagent.get(this.url.url).set(this.config['browser']).end((err, response) => {
-                valid = Judger.valid_judge(err, response);
-            });
-        return valid;
-    }
+	async website_valid_check() {
+		let valid = true;
+		if (this.proxy.length > 4)
+			await superagent.get(this.url.url).set(this.config['browser']).proxy(this.proxy).end((err, response) => {
+				valid = Judger.valid_judge(err, response);
+			});
+		else
+			await superagent.get(this.url.url).set(this.config['browser']).end((err, response) => {
+				valid = Judger.valid_judge(err, response);
+			});
+		return valid;
+	}
 
-    run(solution) {
-        this.pid = solution.pid;
-        this.sid = solution.sid;
-        this.code = solution.code;
-        this.language = solution.language;
-        this.finished = false;
-        logger.info(`run judger for sid:${this.sid}`);
-        //this.website_valid_check();
-        this.login();
-    }
+	run(solution) {
+		this.pid = solution.pid;
+		this.sid = solution.sid;
+		this.code = solution.code;
+		this.language = solution.language;
+		this.finished = false;
+		logger.info(`run judger for sid:${this.sid}`);
+		//this.website_valid_check();
+		this.login();
+	}
 }
 
 module.exports = Judger;
